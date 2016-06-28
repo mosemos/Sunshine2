d5e500bbd649a824a21c90efa4fc8661475b6f54
mosemos <mosemos12@gmail.com>
2016-06-28 17:35:24 +0300

    matched the code to content providers: lesson 8

--- app/src/androidTest/java/com/mosemos/sunshine/data/TestWeatherContract.java
+++ app/src/androidTest/java/com/mosemos/sunshine/data/TestWeatherContract.java
@@ -15,6 +15,7 @@
  */
 package com.mosemos.sunshine.data;
 
+import android.net.Uri;
 import android.test.AndroidTestCase;
 
 /*
@@ -25,20 +26,20 @@ public class TestWeatherContract extends AndroidTestCase {
 
     // intentionally includes a slash to make sure Uri is getting quoted correctly
     private static final String TEST_WEATHER_LOCATION = "/North Pole";
-    private static final long TEST_WEATHER_DATE = 1419033600L;  // December 20th, 2014
+    private static final String TEST_WEATHER_DATE = "SatDec20";//1419033600L;  // December 20th, 2014
 
     /*
         Students: Uncomment this out to test your weather location function.
      */
-//    public void testBuildWeatherLocation() {
-//        Uri locationUri = WeatherContract.WeatherEntry.buildWeatherLocation(TEST_WEATHER_LOCATION);
-//        assertNotNull("Error: Null Uri returned.  You must fill-in buildWeatherLocation in " +
-//                        "WeatherContract.",
-//                locationUri);
-//        assertEquals("Error: Weather location not properly appended to the end of the Uri",
-//                TEST_WEATHER_LOCATION, locationUri.getLastPathSegment());
-//        assertEquals("Error: Weather location Uri doesn't match our expected result",
-//                locationUri.toString(),
-//                "content://com.example.android.sunshine.app/weather/%2FNorth%20Pole");
-//    }
+    public void testBuildWeatherLocation() {
+        Uri locationUri = WeatherContract.WeatherEntry.buildWeatherLocation(TEST_WEATHER_LOCATION);
+        assertNotNull("Error: Null Uri returned.  You must fill-in buildWeatherLocation in " +
+                        "WeatherContract.",
+                locationUri);
+        assertEquals("Error: Weather location not properly appended to the end of the Uri",
+                TEST_WEATHER_LOCATION, locationUri.getLastPathSegment());
+        assertEquals("Error: Weather location Uri doesn't match our expected result",
+                locationUri.toString(),
+                "content://com.mosemos.sunshine/weather/%2FNorth%20Pole");
+    }
 }
